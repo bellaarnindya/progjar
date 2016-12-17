9d3d6072f3631684648e523d99b5ddccd32214c2
rarasanggita <raras.anggita14@mhs.if.its.ac.id>
2016-12-17 18:57:32 +0700

    nambahin modul sys

--- client.py
+++ client.py
@@ -6,7 +6,14 @@ import sys
 client_socket = socket(AF_INET, SOCK_STREAM)
 client_socket.connect(('localhost', 21))
 
+sys.stdout.write(client.recv(1024))
+namafile = ''
+i = 0
+
 while True:
+	msg = sys.stdin.readline()
+	if " " in msg:
+		nama_file = msg.split(' ',1)[1][:-1]
 	reads, writes, errors = select([client_socket], [], [], 1)
 	if not reads:
 		break
@@ -15,10 +22,17 @@ while True:
 			msg = read.recv(2048)
 			print msg,
 
-while True:
-	command = sys.stdin.readline()
-	client_socket.send(command)
+commands = []
+commands.append('USER sabila\r\n')
+commands.append('PASS rani\r\n')
+#commands.append('QUIT\r\n')
+
+i = 0
+
+while i<len(commands):
+	client_socket.send(commands[i])
 	msg = client_socket.recv(1024)
 	print msg,
+	i+=1
 
 client_socket.close()
