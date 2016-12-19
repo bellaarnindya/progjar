c58eed785e7b7b3bad4d2e43bfb4e62f86047e5b
bellaarnindya <bellaarnindya@gmail.com>
2016-12-19 12:32:42 +0700

    benerin client

--- client.py
+++ client.py
@@ -6,33 +6,22 @@ import sys
 client_socket = socket(AF_INET, SOCK_STREAM)
 client_socket.connect(('localhost', 21))
 
-sys.stdout.write(client.recv(1024))
-namafile = ''
-i = 0
 
 while True:
-	msg = sys.stdin.readline()
-	if " " in msg:
-		nama_file = msg.split(' ',1)[1][:-1]
 	reads, writes, errors = select([client_socket], [], [], 1)
 	if not reads:
 		break
 	else:
 		for read in reads:
-			msg = read.recv(2048)
+			msg = read.recv(1024)
 			print msg,
 
-commands = []
-commands.append('USER sabila\r\n')
-commands.append('PASS rani\r\n')
-#commands.append('QUIT\r\n')
 
-i = 0
-
-while i<len(commands):
-	client_socket.send(commands[i])
+while True:
+	command = sys.stdin.readline()
+	client_socket.send(command)
 	msg = client_socket.recv(1024)
-	print msg,
-	i+=1
+	print msg
+
 
 client_socket.close()
