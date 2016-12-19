bd6d199edda575a1c1d5b66f9b9f1fe6da15c4e4
hlmkmilah <hilmakamilah@gmail.com>
2016-12-19 15:52:48 +0700

    merubah total kodingan client hingga bisa quit dengan inputan

--- client.py
+++ client.py
@@ -1,27 +1,64 @@
-from socket import socket, AF_INET, SOCK_STREAM
-from select import select
+import socket
 import sys
 
 
-client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect(('localhost', 52))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('localhost', 22))
 
+sys.stdout.write(client.recv(1024))
+sys.stdout.write('>>')
 
-while True:
-	reads, writes, errors = select([client_socket], [], [], 1)
-	if not reads:
-		break
-	else:
-		for read in reads:
-			msg = read.recv(1024)
-			print msg,
+data_port = 0
+nama_file = ''
+i = 0
 
+try:
+#	while True:
+#		reads, writes, errors = select([client_socket], [], [], 1)
+#		if not reads:
+#			break
+#		else:
+#			for read in reads:
+#				msg = read.recv(1024)
+#				print msg,
+	while True:
+		command = sys.stdin.readline()
+		if " " in command:
+			nama_file = command.split(' ',1)[1][:-1]
+		if i==0:
+			if "USER" not in command:
+				client.send("USER anonymous")
+				pesan = client.recv(1024)
+				sys.stdout.write(pesan)
+				if "331" in pesan:
+					client.send("PASS ****")
+					pesan = client.recv(1024)
+					sys.stdout.write(pesan)
+					print "Could't to connect server"
+			else:
+				client.send(command)
+				pesan = client.recv(1024)
+				sys.stdout.write(pesan)
+		else:
+			client.send(command)
+			pesan = client.recv(1024)
+			sys.stdout.write(pesan)
+		if "221" in pesan:
+			client.close()
+			sys.exit(0)
+			break
+		if "530" in pesan:
+			client.close()
+			sys.exit(0)
+		sys.stdout.write('>>')
+		i+=1
+#		client_socket.send(command)
+#		msg = client_socket.recv(1024)
+#		print msg
 
-while True:
-	command = sys.stdin.readline()
-	client_socket.send(command)
-	msg = client_socket.recv(1024)
-	print msg
+#client_socket.close()
 
+except KeyboardInterrupt:
+	client.close()
 
-client_socket.close()
+	sys.exit(0)
